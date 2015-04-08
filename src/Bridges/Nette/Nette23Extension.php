6a5b795848e521f6bc96820a85341d788a6ed903
Martin Malek <martin.malek@pagewiser.com>
2015-04-08 19:55:12 +0200

    Nette extension does not require client parameter

--- src/Bridges/Nette/Nette23Extension.php
+++ src/Bridges/Nette/Nette23Extension.php
@@ -31,14 +31,7 @@ class Nette23Extension extends Nette\DI\CompilerExtension
 		$connection = $container->addDefinition($this->prefix('connection'))
 			->setClass('Pagewiser\Idn\Nette\Api', array($config['apiKey'], $config['apiSecret']))
 			->addSetup('setApiUrl', array($config['apiHost']))
-			->addSetup('setImageUrl', array($config['imageHost']))
-			->addSetup('loginClient', array($config['client']));
-
-		if (!empty($config['user']) && !empty($config['password']))
-		{
-			$connection->addSetup('loginUser', array($config['user'], $config['password'], (bool) $config['lazy']));
-		}
-
+			->addSetup('setImageUrl', array($config['imageHost']));
 
 		if ($useProfiler) {
 			$panel = $container->addDefinition($this->prefix('panel'))
